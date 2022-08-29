99907b0163877ecf047755ddea6106ff50356e19
bgarcia95 <bgarcia.95@outlook.com>
2022-08-29 15:50:36 -0600

    inject js into html

--- src/HelloWorldApp.jsx
+++ src/HelloWorldApp.jsx
@@ -3,10 +3,20 @@
 // };
 // export default App;
 
+const newMessage = {
+  message: "Hello World",
+  title: "Brayan",
+};
+
+const getResult = (a, b) => a + b;
+
 export const HelloWorldApp = () => {
   return (
     <>
-      <h1>Hello World App</h1>
+      {/* <h1>{getResult(1, 10)}</h1> */}
+      {/* <code>{JSON.stringify(newMessage)}</code> */}
+      <h1>Brayan</h1>
+      <p>I'm a subtitle</p>
     </>
   );
 };
