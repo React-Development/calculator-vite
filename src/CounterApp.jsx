0e19713a61e6e28fcd992d788343a7501d4cc64a
bgarcia95 <bgarcia.95@outlook.com>
2022-09-01 12:16:59 -0600

    add operations to add, minus and reset button

--- src/CounterApp.jsx
+++ src/CounterApp.jsx
@@ -4,8 +4,17 @@ import { useState } from "react";
 export const CounterApp = ({ value }) => {
   const [counter, setCounter] = useState(value);
 
-  const handleAdd = () => {
-    setCounter((prevState) => prevState + 1);
+  const handleOperation = (e, operation) => {
+    switch (operation) {
+      case "add":
+        return setCounter((c) => c + 1);
+      case "decrease":
+        return setCounter((c) => c - 1);
+      case "reset":
+        return setCounter(value);
+      default:
+        return;
+    }
   };
 
   return (
@@ -13,7 +22,9 @@ export const CounterApp = ({ value }) => {
       <h1>CounterApp</h1>
       <h2> {counter} </h2>
 
-      <button onClick={handleAdd}>+1</button>
+      <button onClick={(e) => handleOperation(e, "add")}>+1</button>
+      <button onClick={(e) => handleOperation(e, "decrease")}>-1</button>
+      <button onClick={(e) => handleOperation(e, "reset")}>Reset</button>
     </>
   );
 };
