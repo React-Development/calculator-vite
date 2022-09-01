7cbd0092af6ebe1f046ecb13c732336f1fe910e4
bgarcia95 <bgarcia.95@outlook.com>
2022-09-01 01:38:41 -0600

    import and use counter app component

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./styles.css";
 
-import { HelloWorldApp } from "./HelloWorldApp";
+// import { HelloWorldApp } from "./HelloWorldApp";
+import { CounterApp } from "./CounterApp";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <HelloWorldApp />
+    {/* <HelloWorldApp /> */}
+    <CounterApp value={20} />
   </React.StrictMode>
 );
