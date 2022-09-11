4b7c59a961830d124df77ea78e4fd5bed4172b9d
bgarcia95 <bgarcia.95@outlook.com>
2022-09-10 21:43:48 -0600

    add demo test

--- tests/demo.test.js
+++ tests/demo.test.js
@@ -1,5 +1,16 @@
-test('This test must not fail', () => {
-  if (1 === 0) {
-    throw new Error('You cant divide by zero')
-  }
-});
+
+describe('Tests on <DemoComponent />', () => {
+
+  test('This test must not fail', () => {
+    // 1. start it
+    const message1 = 'Hello World';
+
+    // 2. stimulate
+    const message2 = message1.trim();
+
+
+    // 3. watch expected behavior
+    expect(message1).toBe(message2);
+  });
+
+})
