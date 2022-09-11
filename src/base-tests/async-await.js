7907f9d28fc968f045dc6ba25bd8025c305ff9df
bgarcia95 <bgarcia.95@outlook.com>
2022-09-11 16:16:15 -0600

    add async await tests

--- src/base-tests/async-await.js
+++ src/base-tests/async-await.js
@@ -1,6 +1,6 @@
 export const getImage = async () => {
   try {
-    const apiKey = 'DcFVDfqadxnV2cRtAGwr8Zdfw9nEOQSE';
+    const apiKey = 'DcFVDfqadxnV2cRtAGwr8Zdfw9nEOQSEy';
     const resp = await fetch(`https://api.giphy.com/v1/gifs/random?api_key=${apiKey}&tag=&rating=g`);
     const { data } = await resp.json();
 
--- tests/base-tests/11-async-await.test.js
+++ tests/base-tests/11-async-await.test.js
@@ -3,9 +3,16 @@ import { getImage } from "../../src/base-tests/async-await"
 describe('tests on 11-async-await', () => {
 
   test('getImage must return image URL', async () => {
-    const url = await getImage();
-    // console.log(url);
+    const resp = await getImage();
 
-    expect(typeof url).toBe('string');
+    expect(typeof resp).toBe('string');
+
+  })
+
+  test('getImage must not return image URL if no Api Key', async () => {
+    const resp = await getImage();
+
+    expect(resp).toBe('Image not found');
+
   })
 })
