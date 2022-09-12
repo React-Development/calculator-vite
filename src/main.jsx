f8138dbd62f9d961080d41a159f0ec62858ff084
bgarcia95 <bgarcia.95@outlook.com>
2022-09-12 11:00:43 -0600

    pass title as a prop

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,11 @@ import ReactDOM from "react-dom/client";
 import "./styles.css";
 
 import { CounterApp } from "./CounterApp";
+import { HelloWorldApp } from "./HelloWorldApp";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <CounterApp value={10} />
+    {/* <CounterApp value={10} /> */}
+    <HelloWorldApp title="Hello, I'm Goku" />
   </React.StrictMode>
 );
