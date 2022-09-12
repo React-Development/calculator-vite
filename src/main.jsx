f91ac6111d74c31cab08bbceabecf60e688d5c8e
bgarcia95 <bgarcia.95@outlook.com>
2022-09-12 16:42:51 -0600

    enable CouterApp component

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,7 @@ import { HelloWorldApp } from "./HelloWorldApp";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    {/* <CounterApp value={10} /> */}
-    <HelloWorldApp title="Hello, I'm Goku" />
+    <CounterApp value={100} />
+    {/* <HelloWorldApp title="Hello, I'm Goku" /> */}
   </React.StrictMode>
 );
