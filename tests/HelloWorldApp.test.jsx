04e2b40914f275a2191110f4e648845f8bbf0dea
bgarcia95 <bgarcia.95@outlook.com>
2022-09-12 16:15:05 -0600

    add new tests

--- tests/HelloWorldApp.test.jsx
+++ tests/HelloWorldApp.test.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { getAllByText, render } from "@testing-library/react";
 
 import { HelloWorldApp } from "../src/HelloWorldApp";
 
@@ -13,11 +13,26 @@ describe("tests on HelloWorldApp", () => {
 
   test("should show title on h1", () => {
     const title = "Hello, I'm Goku";
-    const { container, getByText } = render(<HelloWorldApp title={title} />);
+    const { container, getByText, getByTestId } = render(
+      <HelloWorldApp title={title} />
+    );
 
     expect(getByText(title)).toBeTruthy();
 
-    const h1 = container.querySelector("h1");
-    expect(h1.innerHTML).toContain(title);
+    // const h1 = container.querySelector("h1");
+    // expect(h1.innerHTML).toContain(title);
+
+    expect(getByTestId("test-title").innerHTML).toContain(title);
+  });
+
+  test("should show subtitle sent by props", () => {
+    const title = "Hello, I'm Goku";
+    const subtitle = "I'm a subtitle";
+
+    const { getByText, getAllByText } = render(
+      <HelloWorldApp title={title} subtitle={subtitle} />
+    );
+
+    expect(getAllByText(subtitle).length).toBe(2);
   });
 });
