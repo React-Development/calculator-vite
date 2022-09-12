33c309524652f4f9152caef46b22d8b9c8f80b62
bgarcia95 <bgarcia.95@outlook.com>
2022-09-12 17:03:05 -0600

    add new tests

--- tests/CounterApp.test.jsx
+++ tests/CounterApp.test.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 
-import { render, screen } from "@testing-library/react";
+import { fireEvent, render, screen } from "@testing-library/react";
 
 import { CounterApp } from "../src/CounterApp";
 
 describe("tests on CounterApp", () => {
-  const value = 100;
+  const value = 10;
 
   test("should match snapshot", () => {
     const { container } = render(<CounterApp value={value} />);
@@ -14,7 +14,30 @@ describe("tests on CounterApp", () => {
   });
 
   test("should show initial value of 100", () => {
-    render(<CounterApp value={value} />);
+    render(<CounterApp value={100} />);
     expect(screen.getByText(100)).toBeTruthy();
+  });
+
+  test("should increment with +1 button", () => {
+    render(<CounterApp value={value} />); // test subject
+    fireEvent.click(screen.getByText("+1")); // stimulus
+    expect(screen.getByText("11")).toBeTruthy(); // asssertion (expected behavior)
+  });
+
+  test("should decrease with -1 button", () => {
+    render(<CounterApp value={value} />);
+    fireEvent.click(screen.getByText("-1"));
+    expect(screen.getByText("9")).toBeTruthy();
+  });
+
+  test("should work reset button", () => {
+    render(<CounterApp value={355} />);
+    fireEvent.click(screen.getByText("+1"));
+    fireEvent.click(screen.getByText("+1"));
+    fireEvent.click(screen.getByText("+1"));
+    // fireEvent.click(screen.getByText("Reset"));
+    fireEvent.click(screen.getByRole("button", { name: "btn-reset" }));
+
+    expect(screen.getByText(355)).toBeTruthy();
   });
 });
